6156d0505ad1209b2022e71e34169bfa80d64e09
w8f <haya22aws@gmail.com>
2022-07-03 15:10:05 +0900

    :art: Homeページのレイアウト修正

--- front/src/page/Home/Home.page.tsx
+++ front/src/page/Home/Home.page.tsx
@@ -1,9 +1,16 @@
-import { FC } from "react";
+import { FC, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const HomePage: FC = () => {
+  const navigate = useNavigate();
+  const toAbout = useCallback(() => {
+    navigate("/about");
+  }, [navigate]);
+
   return (
-    <>
+    <div style={{ textAlign: "center" }}>
       <h1>Home Page</h1>
-    </>
+      <button onClick={toAbout}>Aboutページへ遷移する。</button>
+    </div>
   );
 };
